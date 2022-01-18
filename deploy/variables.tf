2d568a7b59dcb4cd5819e4ec89f67fabbf590d7e
Ivan Legorreta <ilegorreta@hotmail.com>
2022-01-18 13:05:56 -0600

    Added RDS credentials as environment variables in TF Cloud

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -1,23 +1,23 @@
 variable "rds_id" {
   type        = string
-  default     = "bankaya-db-dev"
   description = "Type a name for your DB instance. The name must be unique across all DB instances owned by your AWS account in the current AWS Region"
+  sensitive   = true
 }
 
 variable "rds_db_name" {
   type        = string
-  default     = "bankaya_db"
   description = "The name of the database to create when the DB instance is created"
+  sensitive   = true
 }
 
 variable "rds_user" {
   type        = string
-  default     = "postgres"
   description = "RDS Master username"
+  sensitive   = true
 }
 
 variable "rds_password" {
   type        = string
-  default     = "BankayaTest567"
   description = "RDS Master password"
+  sensitive   = true
 }
