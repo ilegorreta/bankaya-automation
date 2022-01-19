3383eb0da3fb3724550dabd47d959a1f28c1e2be
Ivan Legorreta <ilegorreta@hotmail.com>
2022-01-18 22:09:19 -0600

    Added ETL pipeline to read from RDS and DynamoDB and insert into Snowflake

--- src/etl_pipeline.py
+++ src/etl_pipeline.py
@@ -2,43 +2,102 @@
 # -*- coding: utf-8 -*-
 import os
 import psycopg2
+import numpy as np
 import pandas as pd
+import boto3
 from sqlalchemy import create_engine
-# import snowflake.connector
-# from snowflake.connector.pandas_tools import write_pandas
+import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
 
 def get_postgres_connection(database, host, user, password, port):
     connection = psycopg2.connect(host=host, user=user, password=password, database=database, port=port)
     return connection
 
-def main():
-    # Connect to DB
-    # conn = snowflake.connector.connect(
-    #     user = os.getenv("SNOWFLAKE_USER"),
-    #     password = os.getenv("SNOWFLAKE_PASSWORD"),
-    #     account = os.getenv("SNOWFLAKE_ACCOUNT"),
-    #     warehouse = os.getenv("SNOWFLAKE_WAREHOUSE"),
-    #     )
-    # conn.cursor().execute('USE ROLE SYSADMIN')
-    # conn.cursor().execute("CREATE WAREHOUSE IF NOT EXISTS test_warehouse")
-    # conn.cursor().execute("USE WAREHOUSE test_warehouse")
-    # conn.cursor().execute("CREATE DATABASE IF NOT EXISTS bankaya_db")
-    # conn.cursor().execute("USE DATABASE bankaya_db")
-    # conn.cursor().execute("CREATE SCHEMA IF NOT EXISTS test_schema")
-    # conn.cursor().execute("USE SCHEMA test_schema")
+def build_snowflake_create_table_command(df, table):
+    """Helper function to construct the string required to create Snowflake tables"""
+    # String to specify the CREATE TABLE statement 
+    create_table_str = f'CREATE OR REPLACE TABLE "{table}" ( '
+    df_types = df.dtypes.to_dict()
+    # Iterate through pandas columns data types to append to the 'CREATE TABLE' string 
+    for key, val in df_types.items():
+        if val == np.int64:
+            val = "INTEGER"
+        elif val == np.float64:
+            val = "FLOAT"
+        else:
+            val = "STRING"
+        create_table_str += f'"{key}": {val}, '
+    # Remove trailing comma and white space
+    create_table_str = create_table_str[:-2]
+    # Close Parenthesis for the 'CREATE TABLE' statement
+    create_table_str += ' )'
+    return create_table_str
 
+def rds_etl_pipeline(conn):
+    """ETL pipeline to retrieve data from RDS into Pandas and then insert into Snowflake tables"""
     try:
+        # Get Snowflake Credentials from environment variables
         host = os.getenv("RDS_HOST")
         user = os.getenv("RDS_USER")
         password = os.getenv("RDS_PASSWORD")
         db = os.getenv("RDS_DB")
         port = os.getenv("RDS_PORT")
-        engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
+        # Create engine to connect to Postgres RDS
+        engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}", pool_recycle=3600)
         tables = ['customer', 'item', 'orders']
-        # Connect to PostgreSQL server
-        dbConnection = engine.connect()
         for table in tables:
-            df = pd.read_sql(f"select * from {table}", dbConnection)
+            # Read data from RDS table and load it as a Pandas Dataframe
+            df = pd.read_sql_query(f'select * from {table}', con=engine)
+            create_table_str = build_snowflake_create_table_command(df, table)
+            # Execute CREATE TABLE command into Snowflake
+            conn.cursor().execute(create_table_str)
+            # Insert Dataframe records into Snowflake table
+            write_pandas(conn, df, table_name=table)
+    except Exception as e:
+        print(f"ERROR: {e}")
+
+def dynamodb_etl_pipeline(conn):
+    """ETL pipeline to retrieve data from DynamoDB into Pandas and then insert into Snowflake tables"""
+    try:
+        TABLE_NAMES = ['customers_data', 'items_data', 'items_bought_data']
+        dynamo = boto3.resource("dynamodb")
+        for table_name in TABLE_NAMES:
+            table = dynamo.Table(table_name)
+            # Get DynamoDB data
+            dynamo_data_list = table.scan()
+            # Convert dynamoDB list of dictionaries into Pandas Dataframe
+            df = pd.DataFrame(dynamo_data_list['Items'])
+            # Add naming suffix to differenciate Snowflake tables coming from RDS from the ones coming from DynamoDB
+            snowflake_table_name = table_name + '_dynamodb'
+            create_table_str = build_snowflake_create_table_command(df, snowflake_table_name)
+            # Execute CREATE TABLE command into Snowflake
+            conn.cursor().execute(create_table_str)
+            # Insert Dataframe records into Snowflake table
+            write_pandas(conn, df, table_name=table)
+    except Exception as e:
+        print(f"ERROR: {e}")
+
+def main():
+    """Function to extract data from RDS and DynamoDB and upload them into Snowflake"""
+    try:
+        # Connect to Snowflake DB
+        conn = snowflake.connector.connect(
+            user = os.getenv("SNOWFLAKE_USER"),
+            password = os.getenv("SNOWFLAKE_PASSWORD"),
+            account = os.getenv("SNOWFLAKE_ACCOUNT"),
+            warehouse = os.getenv("SNOWFLAKE_WAREHOUSE"),
+            )
+        # Create WH, DB and Schema
+        conn.cursor().execute('USE ROLE SYSADMIN')
+        conn.cursor().execute("CREATE WAREHOUSE IF NOT EXISTS test_warehouse")
+        conn.cursor().execute("USE WAREHOUSE test_warehouse")
+        conn.cursor().execute("CREATE DATABASE IF NOT EXISTS bankaya_db")
+        conn.cursor().execute("USE DATABASE bankaya_db")
+        conn.cursor().execute("CREATE SCHEMA IF NOT EXISTS test_schema")
+        conn.cursor().execute("USE SCHEMA test_schema")
+        # Call ETL functions from each DB engine
+        rds_etl_pipeline(conn)
+        dynamodb_etl_pipeline(conn)
     except Exception as e:
         print(f"ERROR: {e}")
 
--- src/upload_nosql_data.py
+++ src/upload_nosql_data.py
@@ -15,8 +15,11 @@ ITEM_TABLE = dynamo.Table(ITEM_TABLE_NAME)
 ORDERS_TABLE = dynamo.Table(ORDERS_TABLE_NAME)
 
 def main():
+    """Read local JSON files and upload the data into DynamoDB tables"""
     nosql_files = ["customer.json", "item.json", "orders.json"]
+    # Iterate through the each local file
     for file in nosql_files:
+        # Read JSON file as Python Dict
         with open(f"data/nosql/{file}") as json_file:
             data_list = json.load(json_file)
         if file == "customer.json":
@@ -25,10 +28,10 @@ def main():
             table = ITEM_TABLE
         else:
             table = ORDERS_TABLE
+        # Insert dictionary records into DynamoDB
         for record in data_list:
-        # Float types are not supported by DynamoDB SDK. Should use Decimal types instead
+            # Float types are not supported by DynamoDB SDK. Should use Decimal types instead
             table.put_item(Item=json.loads(json.dumps(record), parse_float=Decimal))
-    
     print("NOSQL data successfully uploaded into DynamoDB tables!")
 
 if __name__ == "__main__":
